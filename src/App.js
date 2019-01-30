111a6c8d2311d5c63f103d6d9e39e4a091ecf6d7
CHRUSUS-YOGA\Tobias <chrusu@gmail.com>
2019-01-30 21:57:21 +0100

    separating data-collection and rendering from search-component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,65 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import UserSerach from './components/userSearch'
+import UserSerach from './components/user-search/userSearch';
+import axios from 'axios';
+
+const GIT_USER_SEARCH = 'https://api.github.com/search/users?q='
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state= {
+      data: []
+    }
+  }
+
+  getUsers = (searchTerm) => {
+    console.log('searchTerm: ', searchTerm);
+    let url = `${GIT_USER_SEARCH}${searchTerm}`;
+      axios.get(url)
+      .then((response) => {
+        // handle success
+        const items = response.data.items;
+        this.setState({data: items})
+        return items;
+
+      })
+      .catch((error) => {
+        // handle error
+        console.log(error);
+      })
+      .then(() => {
+        // always executed
+      }); 
+  }
+
+  
+  handleMouseOver = (selected) => {
+    console.log('handleMouseOver: ', selected);
+  }
+
+  renderItem = (item, i, selected)=> {
+    let className = `user-item ${i===selected ? 'selected':''}`
+    console.log(className, i, selected);
+    return (
+      <div key={i} className={className} onMouseEnter={() => this.handleMouseOver(i)} >
+        <img className={"user-image"} src={item.avatar_url} alt={item.login} />
+        <div className={"name"}>
+          {item.login}
+        </div>
+      </div>
+    )
+  };
+
+  handleEnter = (login) => {
+    window.location.href = `https://github.com/${login}`;
+  }
+
   render() {
     return (
       <div className="App">
-        <UserSerach></UserSerach>
+        <UserSerach renderItem={this.renderItem} getData={this.getUsers} data={this.state.data} handleEnter={this.handleEnter}></UserSerach>
         <div>fasdfsadf</div>
       </div>
     );
--- src/components/user-search/userSearch.js
+++ src/components/user-search/userSearch.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import './styles.css';
-import axios from 'axios';
 
-const GIT_USER_SEARCH = 'https://api.github.com/search/users?q='
+
 let timeout;
 let nameInput;
 
@@ -12,7 +11,6 @@ export default class UserSearch extends React.Component {
     this.state = {
       selected: '',
       searchTerm: '',
-      displayItems: []
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -23,32 +21,13 @@ export default class UserSearch extends React.Component {
     this.nameInput.focus();
   }
 
-  getUsers = (searchTerm) => {
-    //trigger search after a timeout, so not every keypress leads to a search
-    clearTimeout(timeout);
-    timeout = setTimeout(()=>{
-      axios.get(`${GIT_USER_SEARCH}${searchTerm}`)
-      .then((response) => {
-        // handle success
-        const items = response.data.items;
-        this.setState({displayItems: items})
-        this.setState({selected: 0})
-        console.log(response);
-      })
-      .catch((error) => {
-        // handle error
-        console.log(error);
-      })
-      .then(() => {
-        // always executed
-      });
-    },500);
 
-  }
 
   handleKeyPress = (event) => {
     const { key } = event;
-    const itemCount = this.state.displayItems.length;
+    const { data } = this.props;
+    const itemCount = data.length;
+    //debugger;
     console.log("itemcount: ", itemCount);
     const { selected } = this.state;
     if(key === "ArrowDown" || key === "ArrowUp")
@@ -67,46 +46,42 @@ export default class UserSearch extends React.Component {
       }
     }
     else if(key === "Enter"){
-      window.location.href = `https://github.com/${this.state.displayItems[selected].login}`;
+      this.props.handleEnter(this.props.data[this.state.selected].login);
       //redirect to user profile
     }
   }
 
   handleChange = (event) => {
+    const { getData } = this.props;
     const { value } = event.target;
     if(value !== this.state.searchTerm){
       console.log('handleChange')
       this.setState({ searchTerm: value });
       if(value.length > 2){
-        this.getUsers(value);
+            //trigger search after a timeout, so not every keypress leads to a search
+        clearTimeout(timeout);
+        timeout = setTimeout(()=>{
+          getData(this.state.searchTerm);
+          this.setState({selected: 0})
+        }, 500);
       }else{
         this.setState({displayItems: []});
         this.setState({selected: 0});
       }
     }
-    // console.log(`Typed:`, value);
   }
 
-  renderItem = (item, i)=> {
-    let className = `user-item ${i===this.state.selected ? 'selected':''}`
-    console.log(className, i, this.state.selected);
-    return (
-      <div key={i} className={className}>
-        <img className={"user-image"} src={item.avatar_url} alt={item.login} />
-        <div className={"name"}>
-          {item.login}
-        </div>
-      </div>
-    )
-  };
+
+
   render() {
     const { searchTerm } = this.state;
+    const { data, renderItem } = this.props;
     let classNames = ["search-entries"];
     classNames = classNames.concat(["empty"]);
     return (
       <div className={"search-bar"} onKeyUp={this.handleKeyPress.bind(this)}>
         <input type="text" value={searchTerm} onChange={this.handleChange.bind(this)} ref={(input) => { this.nameInput = input; }}/>
-        <div className={classNames.join(' ')}>{this.state.displayItems.map((item, i) => this.renderItem(item, i))}</div>
+        <div className={classNames.join(' ')}>{data.map((item, i) => renderItem(item, i, this.state.selected))}</div>
       </div>
     );
   }
